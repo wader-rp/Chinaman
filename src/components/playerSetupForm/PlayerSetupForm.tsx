6ba0c5c6b8fb2281449abe621f2b747736d19d3d
wader-rp <waddder@gmail.com>
2023-09-18 23:39:11 +0200

    Added generic function for replacing players values

--- src/components/playerSetupForm/PlayerSetupForm.tsx
+++ src/components/playerSetupForm/PlayerSetupForm.tsx
@@ -8,21 +8,38 @@ type playerProps = {
 };
 
 export const PlayerSetupForm = ({ id }: playerProps) => {
-  const { setPlayers, players } = useGameContext();
+  const { changePlayerProperty, getPlayerById } = useGameContext();
 
-  const [isHumanSelect, setIsHumanSelect] = useState(false);
+  const { isHuman, playerName } = getPlayerById(id);
+
   const onOptionChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setIsHumanSelect(e.target.value === "human");
+    changePlayerProperty(id, "isHuman", e.target.value === "human");
   };
-  //   useEffect(() => {}, [isHumanSelect]);
-  console.log(isHumanSelect);
+  const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    changePlayerProperty(id, "playerName", e.target.value);
+  };
+
+  useEffect(() => {
+    const newName = isHuman
+      ? playerName.replace("BOT ", "")
+      : `BOT ${playerName}`;
+    changePlayerProperty(id, "playerName", newName);
+    console.log(newName);
+  }, [isHuman]);
+
   return (
     <div className="playerSetup-form">
-      <h2>{`Are you a human ? ${id}`}</h2>
+      <h2>{`Are you a human ? ${name}`}</h2>
       <select name="isHuman" id="isHuman" onChange={onOptionChange}>
         <option value="pc">No, let my computer play</option>
         <option value="human">Yes!</option>
       </select>
+      <span>{playerName}</span>
+      {isHuman && (
+        <div>
+          <input value={playerName} onChange={onNameChange} />
+        </div>
+      )}
     </div>
   );
 };
